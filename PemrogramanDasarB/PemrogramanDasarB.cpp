00f46e9a868efb40c462551abc79cc569c5f57a3
ulfhiraa <studyiraa@gmail.com>
2023-03-22 17:20:35 +0700

    menambahkan float

---
+++ PemrogramanDasarB/PemrogramanDasarB.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+float LuasPersegipanjang(float p, float l) {
+    return p * l;
+}
+float LuasLingkarang(float r) {
+    return 3.14 * r * r;
+}
+
+float LuasSegitiga(float a, float t) {
+    return 0.5 * a * t;
+}
